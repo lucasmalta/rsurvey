fed577b4efbaa8f977efffed6b750d8a187b13fd
Malta <LMALTA@volvocars.com>
2018-11-09 14:41:35 +0100

    Adjusted for new filtering

--- unit_test.R
+++ unit_test.R
@@ -11,51 +11,59 @@ mydata <- read.delim("Y:\\Data\\Questionnaire\\unit_test\\System Safety Survey17
 
 ######################### Likert
 
-# TC all
-data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\likertall.csv", sep = ',')
-x = likplot(mydata)
-write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
-result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
-expect_equal(result,data_all)
 
+test_that("T1", {
+  # TC all
+  data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\likertall.csv", sep = ',')
+  x = likplot(mydata)
+  write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
+  result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
+  expect_equal(result,data_all)
+})
 
 # TC manager
-data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\likertmanager.csv", sep = ',')
-mydata_filt = questfilt(mydata,'manager','all',FALSE)
-x = likplot(mydata_filt)
-write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
-result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
-expect_equal(result,data_all)
+test_that("T2", {
+  data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\likertmanager.csv", sep = ',')
+  mydata_filt = questfilt(mydata,'manager','all','keep')
+  x = likplot(mydata_filt)
+  write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
+  result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
+  expect_equal(result,data_all)
+})
 
 
 # TC non manager
-data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\likertnonmanager.csv", sep = ',')
-mydata_filt = questfilt(mydata,'non_manager','all',FALSE)
-x = likplot(mydata_filt)
-write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
-result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
-expect_equal(result,data_all)
-
+test_that("T3", {
+  data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\likertnonmanager.csv", sep = ',')
+  mydata_filt = questfilt(mydata,'non_manager','all','keep')
+  x = likplot(mydata_filt)
+  write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
+  result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
+  expect_equal(result,data_all)
+})
 
 
 #################### Sankey
 # TC all
-q = "I know where to find information on instructions, guidelines, processes or templates"
-data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\sankeyall.csv", sep = ',')
-mydata_filt = questfilt(mydata,'all','all',TRUE)
-x = sankeyplot(mydata_filt,q)
-write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
-result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
-expect_equal(result,data_all)
+test_that("T4", {
+  q = "I know where to find information on instructions, guidelines, processes or templates"
+  data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\sankeyall.csv", sep = ',')
+  x = sankeyplot(mydata,q)
+  write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
+  result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
+  expect_equal(result,data_all)
+})
 
 
 # TC non manager
-data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\sankeynonmanager.csv", sep = ',')
-mydata_filt = questfilt(mydata,'non_manager','all',TRUE)
-x = sankeyplot(mydata_filt,q)
-write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
-result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
-expect_equal(result,data_all)
-
+test_that("T5", {
+  data_all = read.delim("Y:\\Data\\Questionnaire\\unit_test\\sankeynonmanager.csv", sep = ',')
+  q = "I know where to find information on instructions, guidelines, processes or templates"
+  mydata_filt = questfilt(mydata,'non_manager','all','fix')
+  x = sankeyplot(mydata_filt,q)
+  write.csv(x,"Y:\\Data\\Questionnaire\\unit_test\\temp.csv")
+  result = read.delim("Y:\\Data\\Questionnaire\\unit_test\\temp.csv", sep = ',')
+  expect_equal(result,data_all)
+})
 
 
